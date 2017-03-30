e4c39a4d8f60bfc2f5e747f831d6294bd0320b56
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-03-29 20:41:21 -0700

    add html template and css style

---
+++ isafe_viewer/web_app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, session, redirect
+import os
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    return render_template('main.html')
