2f9685909dc71f135177c71cc2c453bd7d205631
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-03-31 10:48:37 -0700

    add loading procedure

--- isafe_viewer/web_app.py
+++ isafe_viewer/web_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import os
+import time
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def main():
     if request.method == 'POST':
         print(request.form)
+        time.sleep(5)
         return jsonify(img_url_1='something.jpg', img_url_2='something2.jpg')
     else:
         return render_template('main.html')
