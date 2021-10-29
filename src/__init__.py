351106508534ec287758806d155cac9d1f445521
alek0991 <alek0991@gmail.com>
2021-10-28 22:12:22 -0400

    v1.1.0

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'Ali Akbari et al'
-__email__ = 'Ali_Akbari@hms.harvard.edu'
-__version__ = '0.1.7'
-
--- src/__main__.py
+++
@@ -1,6 +0,0 @@
-from .isafe import run
-
-if __name__ == '__main__':
-    run()
-    
-    
